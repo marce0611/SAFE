2f548e98966a1b008f5bfe4ea8e0409b6f24cd83
Marcela Faúndez <marce.faundezr@gmail.com>
2018-11-25 00:00:39 -0300

    Algunas cosas

---
+++ SAFE/AccesoWebService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SAFE
+{
+    public static class AccesoWebService
+    {
+
+        public static ServicioWeb.ServicioPaginaWebClient acceso
+        {
+            get
+            {
+                try
+                {
+                    return new ServicioWeb.ServicioPaginaWebClient();
+                }
+                catch (System.ServiceModel.EndpointNotFoundException ex)
+                {
+                    throw ex;
+                }
+            }
+        }
+    }
+}
