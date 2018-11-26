e20fdec0fd10981515734462a268c1f0aff01c30
IvanCamposCuevas <38637360+IvanCamposCuevas@users.noreply.github.com>
2018-11-26 14:23:15 -0300

    Clase para nombrar las sesiones

---
+++ SAFE/NombresSesiones.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SAFE
+{
+    public static class NombresSesiones
+    {
+
+        public static string DatosUsuario
+        {
+            get { return "datosUsuario"; }
+        }
+
+    }
+}
